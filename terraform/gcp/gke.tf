8350214e030dd0051006aa63d205884ff9131e9a
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-05-16 16:32:41 -0300

    Drata Compliance Recommendations (Grouped)

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -4,6 +4,7 @@ data "google_compute_zones" "available_zones" {
 }
 
 resource "google_container_cluster" "workload_cluster" {
+  # Drata: Kubernetes provides an additional layer of security for sensitive data, such as Secrets, stored in etcd. Using this functionality, you can use a key managed by your Cloud provider to encrypt data at the etcd layer. This encryption protects against attackers who gain access to an offline copy of etcd. Ensure that [google_container_cluster.database_encryption.state] properties are correctly defined for encrypting secrets
   name               = "terragoat-${var.environment}-cluster"
   logging_service    = "none"
   location           = var.region
