cb45555cc6e81ca47183a36895d735e292f64f16
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-05-16 13:18:12 -0300

    Drata Compliance Recommendations (Grouped)

--- terraform/azure/aks.tf
+++ terraform/azure/aks.tf
@@ -1,4 +1,4 @@
-resource azurerm_kubernetes_cluster "k8s_cluster" {
+resource azurerm_kubernetes_cluster "k8s_cluster" { # Drata:  should be set to any of azure, calico, cilium
   # Drata: Ensure that [azurerm_kubernetes_cluster.api_server_authorized_ip_ranges] is explicitly defined and narrowly scoped to only allow trusted sources to access AKS Control Plane
   dns_prefix          = "terragoat-${var.environment}"
   location            = var.location
