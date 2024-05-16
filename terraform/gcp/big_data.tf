e6d04b96acabc75c2aef31e858abd1f266e73381
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-05-16 14:11:05 -0300

    Drata Compliance Recommendations: Public Access Restricted

--- terraform/gcp/big_data.tf
+++ terraform/gcp/big_data.tf
@@ -14,6 +14,7 @@ resource "google_sql_database_instance" "master_instance" {
     }
     backup_configuration {
       enabled = false
+    # Drata: Ensure that SQL database instance is not publicly accessible by giving it a private IP. Define [google_sql.sql_database_instance.settings.ip_configuration.private_network] to use private IPs while connecting to SQL database instance
     }
   }
 }
