2aeb1f506c5c39ee7a7adbbeef11fbd30ac08f39
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-05-16 16:29:39 -0300

    Drata Compliance Recommendations (Grouped)

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -128,6 +128,7 @@ resource "aws_rds_cluster" "app8-rds-cluster" {
 }
 
 resource "aws_rds_cluster" "app9-rds-cluster" {
+  # Drata: Configure [aws_rds_cluster.availability_zones] to improve infrastructure availability and resilience
   # Drata: Set [aws_rds_cluster.storage_encrypted] to true to ensure transparent data encryption is enabled
   cluster_identifier      = "app9-rds-cluster"
   allocated_storage       = 10
