2fe267601d70958438a66e705bfd56d2f8bf8503
Drata-Action-Bot-Dev <actionbot-drata@drata.com>
2024-05-16 14:11:03 -0300

    Drata Compliance Recommendations: Zone Redundancy Configured

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -63,6 +63,7 @@ resource "aws_rds_cluster" "app4-rds-cluster" {
 }
 
 resource "aws_rds_cluster" "app5-rds-cluster" {
+  # Drata: Configure [aws_rds_cluster.availability_zones] to improve infrastructure availability and resilience
   cluster_identifier      = "app5-rds-cluster"
   allocated_storage       = 10
   backup_retention_period = 15
